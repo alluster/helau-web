fff11e5454635304e4d6381b95962f20235cb0b9
Aleksanteri Heliövaara <aleksanteriheliovaara@Aleksanteris-MacBook-Pro.local>
2021-07-28 21:15:40 +0300

    contentfull added

--- src/components/seo.js
+++ src/components/seo.js
@@ -30,7 +30,6 @@ const SEO = ({ title, description, image, article }) => {
       <meta name="image" content={seo.image} />
 
       {seo.url && <meta property="og:url" content={seo.url} />}
-
       {(article ? true : null) && <meta property="og:type" content="article" />}
 
       {seo.title && <meta property="og:title" content={seo.title} />}
